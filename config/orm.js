41994d6c3c6306404c50cdf0f37611445cadc7b0
David Giles <mrgteaches@live.com>
2019-04-23 10:28:31 -0400

    Initial folder/file set up

---
+++ config/orm.js
@@ -0,0 +1,38 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+var orm = {
+    selectAll: function(tableInput, colToSearch, valOfCol) {                        // Only names have been set up, not functions themselves
+      var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+      connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+        if (err) throw err;
+        console.log(result);
+      });
+    },
+    insertOne: function(whatToSelect, table, orderCol) {
+      var queryString = "SELECT ?? FROM ?? ORDER BY ?? DESC";
+      console.log(queryString);
+      connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
+        if (err) throw err;
+        console.log(result);
+      });
+    },
+    updateOne: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
+      var queryString =
+        "SELECT ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
+  
+      connection.query(
+        queryString,
+        [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
+        function(err, result) {
+          if (err) throw err;
+          console.log(result);
+        }
+      );
+    }
+  };
+  
+
+
+// Export the orm object.
+module.exports = orm;
