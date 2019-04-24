325bfcd7b5e84d2410036d7f4a9e044c271ee43a
David Giles <mrgteaches@live.com>
2019-04-24 09:58:51 -0400

    Added CSS, finalize handlebars, clean up code

--- config/orm.js
+++ config/orm.js
@@ -1,43 +1,5 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
-
-// var orm = {
-    
-//     selectAll: function(req, res) {
-//         connection.query("SELECT * FROM burgers;", function(err, data) {
-//           if (err) throw err;
-      
-            
-//           res.render("index", { tasks: data });
-//         });
-//       },
-
-
-
-
-
-//     insertOne: function(whatToSelect, table, orderCol) {
-//       var queryString = "SELECT ?? FROM ?? ORDER BY ?? DESC";
-//       console.log(queryString);
-//       connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
-//         if (err) throw err;
-//         console.log(result);
-//       });
-//     },
-//     updateOne: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
-//       var queryString =
-//         "SELECT ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
-  
-//       connection.query(
-//         queryString,
-//         [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
-//         function(err, result) {
-//           if (err) throw err;
-//           console.log(result);
-//         }
-//       );
-//     }
-//   };
   
 var orm = 
 {
